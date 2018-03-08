5d8da1254c16f0ae465d7da1a4758504e9ac46a8
browlaw <34523848+browlaw@users.noreply.github.com>
2018-03-08 18:53:04 +0100

    Add files via upload

--- UnserWorkspace/Sotfwareprojekt/src/Grafikrechner/Gui.java
+++ UnserWorkspace/Sotfwareprojekt/src/Grafikrechner/Gui.java
@@ -12,40 +12,29 @@ public class Gui extends JFrame {
 	Steuerung dieSteuerung;
 	
 	
-	//Panels
-	
+	//--------------------------------------------------------------------------------------------------------Panels
 	JPanel jpContentpane;
 	ZeichneFunktion zfZeichneFunktion;
 	JPanel jpFunktionen;
 	JPanel jpAbleitungen;
 	JPanel jpWerte;
 	JPanel jpAusgabe;
-	
-	//JLabel
+	//--------------------------------------------------------------------------------------------------------JLabel
 	JLabel jlFunktion1;
 	JLabel jlXwert1;
 	JLabel jlYwert1;
-	
-	
-	//TextFields
+	//--------------------------------------------------------------------------------------------------------TextFields
 	JTextField jtfFunktion1;
 	JTextField jtfXwert1;
-	
-	//JButton
-	
+	//--------------------------------------------------------------------------------------------------------JButton
 	JButton jbBerechne;
-
-	//JMenu
-	
+	//--------------------------------------------------------------------------------------------------------JMenu	
 	JMenu jmskalierung;
-	JMenu jmsueber;
-	
-	//JMenuBar
+	JMenu jmHilfe;
+	//--------------------------------------------------------------------------------------------------------JMenuBar
 	JMenuBar jmbMeinMenu;
-	
-	//JMenuItem
-	
-	JMenuItem jmiHilfe;
+	//--------------------------------------------------------------------------------------------------------JMenuItem
+	JMenuItem jmiAnleitung;
 	JMenuItem jmiSkalierungEinstellen;
 	
 	
@@ -54,67 +43,40 @@ public class Gui extends JFrame {
 		dieSteuerung = s;
 		
 		
-		//Panel Objekte
-		
+	 //-------------------------------------------------------------------------------------------------------JPanel Objekte	
 		jpContentpane     = new JPanel(new BorderLayout());
 		zfZeichneFunktion = new ZeichneFunktion();
 		jpFunktionen      = new JPanel(new GridLayout(1, 5));
 		jpAbleitungen     = new JPanel(new GridLayout());
 		jpWerte           = new JPanel(new GridLayout(1,1));
-		jpAusgabe         = new JPanel(new GridLayout(2,1));
-		
-		//TextField Objekte
+		jpAusgabe         = new JPanel(new GridLayout(2,1));	
+	 //-------------------------------------------------------------------------------------------------------JTextField Objekte
 		jtfFunktion1 = new JTextField();
-		jtfXwert1    = new JTextField();
-		
-				
-		//Label Objekte
+		jtfXwert1    = new JTextField();		
+	 //-------------------------------------------------------------------------------------------------------JLabel Objekte
 		jlFunktion1 = new JLabel("Funktion: ");
 		jlXwert1    = new JLabel("X: ");
 		jlYwert1    = new JLabel("Y:");
-		
-		//JMenu Objekte
-	
+	 //-------------------------------------------------------------------------------------------------------JMenu Objekte
 		jmskalierung = new JMenu("Skalierung");
-		jmsueber     = new JMenu("Über");
-		
-		
-		//Menubar Objekt 
-				
+		jmHilfe     = new JMenu("Hilfe");	
+	 //-------------------------------------------------------------------------------------------------------JMenuBar Objekte				
 		jmbMeinMenu = new JMenuBar();
-		
-		//Menu Item Objekt
-		
-		jmiHilfe = new JMenuItem("Hilfe");
+	 //-------------------------------------------------------------------------------------------------------JMenuItem Objekte
+		jmiAnleitung = new JMenuItem("Anleitung");
 		jmiSkalierungEinstellen = new JMenuItem("Skalierung Einstellen");
-		
-		
-		//JButton Objekt
-		
+	 //-------------------------------------------------------------------------------------------------------JButton Objekte
 		jbBerechne = new JButton("Berechne");
+	 //-------------------------------------------------------------------------------------------------------Add Befehle
 		
-		
-		//Add befehle ----------------------------------
-		
-		
-	    //JMenu add
-		
+
+	 //-------------------------------------------------------------------------------------------------------JMenu add		
 		jmskalierung.add(jmiSkalierungEinstellen);
-		jmsueber.add(jmiHilfe);
-		
-		
-		//JMenubar add
-		
-		jmbMeinMenu.add(jmsueber);
+		jmHilfe.add(jmiAnleitung);
+	 //-------------------------------------------------------------------------------------------------------JMenubar add
 		jmbMeinMenu.add(jmskalierung);
-		
-		
-	
-		
-		
-		
-		
-		//Panel add
+		jmbMeinMenu.add(jmHilfe);
+	 //-------------------------------------------------------------------------------------------------------Panel add
 		jpFunktionen.add(jlFunktion1);
 		jpFunktionen.add(jtfFunktion1);
 		jpFunktionen.add(jlXwert1);
@@ -126,24 +88,23 @@ public class Gui extends JFrame {
 		jpContentpane.add(jpFunktionen,BorderLayout.NORTH);
 		jpContentpane.add(zfZeichneFunktion,BorderLayout.CENTER);
 		jpContentpane.add(jpAusgabe,BorderLayout.SOUTH);
-		
-		
-		//Action Listener
+	  //-------------------------------------------------------------------------------------------------------Action Listener
 		jbBerechne.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				
 				jlYwert1.setText("Y:"+jtfXwert1.getText());
+				System.out.println(jtfFunktion1.getText());
 			}
 		});	
 		
-		//Fenster Optionen
+	 //---------------------------------------------------------------------------------------------------------Fenster Optionen
 		
 		this.setJMenuBar(jmbMeinMenu);
 		this.setContentPane(jpContentpane);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setSize(200,200);
+		this.pack();
 		
 	}
 	
