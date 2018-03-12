0b45eac09db8084f559b9fa0380b8e3fa6410baf
xitsMichi <36621213+xitsMichi@users.noreply.github.com>
2018-03-12 13:42:38 +0100

    Add files via upload

--- UnserWorkspace/Sotfwareprojekt/src/Grafikrechner/Gui.java
+++ UnserWorkspace/Sotfwareprojekt/src/Grafikrechner/Gui.java
@@ -6,12 +6,16 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 
-public class Gui extends JFrame {
+public class Gui<E> extends JFrame {
 
 	Steuerung dieSteuerung;
 	
-	
+	//--------------------------------------------------------------------------------------------------------FrameAnleitung
+	FrameAnleitung fAnleitung;
+	//--------------------------------------------------------------------------------------------------------JDropdown
+	JComboBox<String> jcbfuktionstypWählen;
 	//--------------------------------------------------------------------------------------------------------Panels
 	JPanel jpContentpane;
 	ZeichneFunktion zfZeichneFunktion;
@@ -20,6 +24,7 @@ public class Gui extends JFrame {
 	JPanel jpWerte;
 	JPanel jpAusgabe;
 	//--------------------------------------------------------------------------------------------------------JLabel
+	JLabel jltest;
 	JLabel jlFunktion1;
 	JLabel jlXwert1;
 	JLabel jlYwert1;
@@ -40,13 +45,14 @@ public class Gui extends JFrame {
 	
 	public Gui(Steuerung s) {
 		
+     //-------------------------------------------------------------------------------------------------------JCombobox
+	    jcbfuktionstypWählen = new JComboBox<>();
+     //-------------------------------------------------------------------------------------------------------Steuerung		
 		dieSteuerung = s;
-		
-		
 	 //-------------------------------------------------------------------------------------------------------JPanel Objekte	
 		jpContentpane     = new JPanel(new BorderLayout());
 		zfZeichneFunktion = new ZeichneFunktion();
-		jpFunktionen      = new JPanel(new GridLayout(1, 5));
+		jpFunktionen      = new JPanel(new GridLayout(1, 6));
 		jpAbleitungen     = new JPanel(new GridLayout());
 		jpWerte           = new JPanel(new GridLayout(1,1));
 		jpAusgabe         = new JPanel(new GridLayout(2,1));	
@@ -54,6 +60,7 @@ public class Gui extends JFrame {
 		jtfFunktion1 = new JTextField();
 		jtfXwert1    = new JTextField();		
 	 //-------------------------------------------------------------------------------------------------------JLabel Objekte
+		jltest = new JLabel("test");
 		jlFunktion1 = new JLabel("Funktion: ");
 		jlXwert1    = new JLabel("X: ");
 		jlYwert1    = new JLabel("Y:");
@@ -69,7 +76,6 @@ public class Gui extends JFrame {
 		jbBerechne = new JButton("Berechne");
 	 //-------------------------------------------------------------------------------------------------------Add Befehle
 		
-
 	 //-------------------------------------------------------------------------------------------------------JMenu add		
 		jmskalierung.add(jmiSkalierungEinstellen);
 		jmHilfe.add(jmiAnleitung);
@@ -77,8 +83,15 @@ public class Gui extends JFrame {
 		jmbMeinMenu.add(jmskalierung);
 		jmbMeinMenu.add(jmHilfe);
 	 //-------------------------------------------------------------------------------------------------------Panel add
+		jcbfuktionstypWählen.addItem("Anzahl Faktoren");
+		jcbfuktionstypWählen.addItem("1");
+		jcbfuktionstypWählen.addItem("2");
+		jcbfuktionstypWählen.addItem("3");
+		jcbfuktionstypWählen.addItem("4");
+		jcbfuktionstypWählen.addItem("5");
 		jpFunktionen.add(jlFunktion1);
 		jpFunktionen.add(jtfFunktion1);
+		jpFunktionen.add(jcbfuktionstypWählen);
 		jpFunktionen.add(jlXwert1);
 		jpFunktionen.add(jtfXwert1);
 		jpFunktionen.add(jbBerechne);
@@ -95,15 +108,56 @@ public class Gui extends JFrame {
 			public void actionPerformed(ActionEvent arg0) {
 				
 				jlYwert1.setText("Y:"+jtfXwert1.getText());
-				System.out.println(jtfFunktion1.getText());
+				
+				if(s.ueberpruefeFunktion(jtfFunktion1.getText().toString())) {
+					
+			switch (jcbfuktionstypWählen.getSelectedIndex()) {
+			
+			case 1:
+				s.zerlegeFunktionsgleichungEins(jtfFunktion1.getText().toString());
+				break;
+			case 2:
+				s.zerlegeFunktionsgleichungZwei(jtfFunktion1.getText().toString());
+				break;
+			case 3:
+				s.zerlegeFunktionsgleichungDrei(jtfFunktion1.getText().toString());
+				break;
+			case 4:
+				s.zerlegeFunktionsgleichungVier(jtfFunktion1.getText().toString());
+				break;
+			case 5:
+				s.zerlegeFunktionsgleichungFuenf(jtfFunktion1.getText().toString());
+				break;
+			default:
+				jtfFunktion1.setText("wähl was aus du idiot");
+				break;
+			}
+				}
+				
+				
+				
+				
 			}
 		});	
+		
+		
+		
+		jmiAnleitung.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Hi");
+			
+				fAnleitung = new FrameAnleitung();
+				
+			}
+		});
 		
 	 //---------------------------------------------------------------------------------------------------------Fenster Optionen
 		
 		this.setJMenuBar(jmbMeinMenu);
 		this.setContentPane(jpContentpane);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
 		this.pack();
 		
 	}
--- UnserWorkspace/Sotfwareprojekt/src/Grafikrechner/Steuerung.java
+++ UnserWorkspace/Sotfwareprojekt/src/Grafikrechner/Steuerung.java
@@ -1,17 +1,286 @@
 package Grafikrechner;
 
+import java.util.ArrayList;
+
 public class Steuerung {
 
 	Gui dieGui;
+	
 	
 	
 	public Steuerung() {
 		
 		dieGui = new Gui(this);
 		dieGui.setVisible(true);
+	
+	}
+	
+public ArrayList<String> zerlegeFunktionsgleichungFuenf(String funktionsgleichung){
+		
+		int index = 0;
+		
+		ArrayList<String> liste = new ArrayList<String>();
+		String s1 = funktionsgleichung;
+		char[] tabelle = s1.toCharArray();
+		
+		for (int i = 1; i < tabelle.length; i++) {
+			
+			if ((tabelle[i] == '+') || (tabelle[i] == '-') ) {
+	     		liste.add(s1.substring(0, i));
+	     	break;	
+	     		
+			}	
+		}
+		
+		index = liste.get(0).length();
+		s1 = s1.substring(index);
+		tabelle = s1.toCharArray();
+				
+		for (int j = 1; j < tabelle.length; j++) {
+
+			if (tabelle[j] == '-' || (tabelle[j] == '+') ) {
+				liste.add(s1.substring(0,j));
+				
+				break;
+			}
+			
+		}
+		
+		
+		index = liste.get(1).length();
+		s1 = s1.substring(index);
+		tabelle = s1.toCharArray();
+		
+	
+		for (int j = 1; j < tabelle.length; j++) {
+
+			if (tabelle[j] == '-' || (tabelle[j] == '+') ) {
+				liste.add(s1.substring(0,j));
+				
+				break;
+			}
+			
+			
+		}
+		
+		
+		index = liste.get(2).length();
+		s1 = s1.substring(index);
+		tabelle = s1.toCharArray();
+		
+		for (int j = 1; j < tabelle.length; j++) {
+
+			if (tabelle[j] == '-' || (tabelle[j] == '+') ) {
+				liste.add(s1.substring(0,j));
+				
+				break;
+			}
+			
+			
+		}
+		
+		
+		index = liste.get(3).length();
+		s1 = s1.substring(index);
+		tabelle = s1.toCharArray();
+		
+		liste.add(s1);
+		
+		System.out.println("Faktoren:");
+		
+		for (int i = 0; i < 4; i++) {
+			
+			System.out.println(liste.get(i));
+			
+		}
+		return liste;
+		
 		
 		
 	}
 	
 	
+	
+	public ArrayList<String> zerlegeFunktionsgleichungVier(String funktionsgleichung){
+		
+		int index = 0;
+		
+		ArrayList<String> liste = new ArrayList<String>();
+		String s1 = funktionsgleichung;
+		char[] tabelle = s1.toCharArray();
+		
+		for (int i = 1; i < tabelle.length; i++) {
+			
+			if ((tabelle[i] == '+') || (tabelle[i] == '-') ) {
+	     		liste.add(s1.substring(0, i));
+	     	break;	
+	     		
+			}	
+		}
+		
+		index = liste.get(0).length();
+		s1 = s1.substring(index);
+		tabelle = s1.toCharArray();
+				
+		for (int j = 1; j < tabelle.length; j++) {
+
+			if (tabelle[j] == '-' || (tabelle[j] == '+') ) {
+				liste.add(s1.substring(0,j));
+				
+				break;
+			}
+			
+		}
+		
+		
+		index = liste.get(1).length();
+		s1 = s1.substring(index);
+		tabelle = s1.toCharArray();
+		
+	
+		for (int j = 1; j < tabelle.length; j++) {
+
+			if (tabelle[j] == '-' || (tabelle[j] == '+') ) {
+				liste.add(s1.substring(0,j));
+				
+				break;
+			}
+			
+			
+		}
+		
+		
+		index = liste.get(2).length();
+		s1 = s1.substring(index);
+		tabelle = s1.toCharArray();
+		
+		liste.add(s1);
+		
+		System.out.println("Faktoren:");
+		
+		for (int i = 0; i < 4; i++) {
+			
+			System.out.println(liste.get(i));
+			
+		}
+		return liste;
+		
+		
+		
+	}
+	
+	public ArrayList<String> zerlegeFunktionsgleichungDrei(String funktionsgleichung){
+		
+		int index = 0;
+		
+		ArrayList<String> liste = new ArrayList<String>();
+		String s1 = funktionsgleichung;
+		char[] tabelle = s1.toCharArray();
+		
+		for (int i = 1; i < tabelle.length; i++) {
+			
+			if ((tabelle[i] == '+') || (tabelle[i] == '-') ) {
+	     		liste.add(s1.substring(0, i));
+	     	break;	
+	     		
+			}	
+		}
+		
+		index = liste.get(0).length();
+		s1 = s1.substring(index);
+		tabelle = s1.toCharArray();
+				
+		for (int j = 1; j < tabelle.length; j++) {
+
+			if (tabelle[j] == '-' || (tabelle[j] == '+') ) {
+				liste.add(s1.substring(0,j));
+				
+				break;
+			}
+			
+		}
+		
+		
+		index = liste.get(1).length();
+		s1 = s1.substring(index);
+		tabelle = s1.toCharArray();		
+		
+		liste.add(s1);
+		
+		System.out.println("Faktoren:");
+		
+		for (int i = 0; i < 3; i++) {
+			
+			System.out.println(liste.get(i));
+			
+		}
+		return liste;
+		
+		
+		
+	}
+	
+	public ArrayList<String> zerlegeFunktionsgleichungZwei(String funktionsgleichung){
+		
+		int index = 0;
+		
+		ArrayList<String> liste = new ArrayList<String>();
+		String s1 = funktionsgleichung;
+		char[] tabelle = s1.toCharArray();
+		
+		for (int i = 1; i < tabelle.length; i++) {
+			
+			if ((tabelle[i] == '+') || (tabelle[i] == '-') ) {
+	     		liste.add(s1.substring(0, i));
+	     	break;	
+	     		
+			}	
+		}
+		
+		index = liste.get(0).length();
+		s1 = s1.substring(index);
+		tabelle = s1.toCharArray();
+						
+		
+		liste.add(s1);
+		
+		System.out.println("Faktoren:");
+		
+		for (int i = 0; i < 2; i++) {
+			
+			System.out.println(liste.get(i));
+			
+		}
+		return liste;
+		
+		
+		
+	}
+	public ArrayList<String> zerlegeFunktionsgleichungEins(String funktionsgleichung){
+		
+		int index = 0;
+		
+		ArrayList<String> liste = new ArrayList<String>();
+		String s1 = funktionsgleichung;
+		char[] tabelle = s1.toCharArray();						
+		
+		liste.add(s1);
+		
+		System.out.println("Faktoren:");
+		
+		for (int i = 0; i < 1; i++) {
+			
+			System.out.println(liste.get(i));
+			
+		}
+		return liste;
+		
+		
+		
+	}
+
+	public boolean ueberpruefeFunktion(String Funktionsgleichung) {
+		
+		return true;
+	}
 }
